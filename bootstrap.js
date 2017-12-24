7f0b6fd1f16d4981280badb1b6f55d23bb47bf86
Chen <chenchu>
2017-12-25 01:31:59 +0800

    1
    11

---
+++ bootstrap.js
@@ -0,0 +1,8 @@
+//= require bootstrap/carousel
+
+// $(document).ready(function() {
+//   $('#carousel-notification').carousel({interval: 2000});
+
+// });
+
+$('carousel').carousel.slide({interval: 100})
